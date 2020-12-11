58c81020d9782b7e1d9637ea6ba3da6c7c6ee4ed
joshuawright89 <73801906+joshuawright89@users.noreply.github.com>
2020-12-10 21:08:28 -0500

    First push/Challenges completed

---
+++ Wk2_Challenges/Greeter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk2_Challenges
+{
+    class Greeter
+    {
+        //"Build a class called Greeter.  This will hold our next few methods."
+        //[[[Done ^^^]]]
+
+        //Inside this new class, build method that takes in a name and outputs (to the console) hello to that person
+        public void TakeNameSayHello(string name)
+        {
+            Console.WriteLine($"Hello there, + {name}!");
+        }
+
+        //Build another method that takes a name and outputs a farewell to that person
+        public void TakeNameSayBye(string name)
+        {
+            Console.WriteLine($"Goodbye, {name}.");
+        }
+
+        //Build a method that outputs, depending on time of day:
+        //a. "Good morning"
+        //b. "Good afternoon"
+        //c. "Good evening"
+        //d. "Good night"
+
+        public void TimeOfDay()
+        {
+            int timeOfDay = DateTime.Now.Hour;
+
+            if (timeOfDay >= 0 && timeOfDay < 12)
+            {
+                Console.WriteLine("Good morning!");
+            }
+            else if (timeOfDay >= 12 && timeOfDay < 18)
+            {
+                Console.WriteLine("Good afternoon!");
+            }
+            else if (timeOfDay >= 18 && timeOfDay < 21)
+            {
+                Console.WriteLine("Good evening.");
+            }
+            else
+            {
+                Console.WriteLine("Nighty night, don't let the bed bugs give you HIV.");
+            }
+
+
+
+        }
+
+
+
+    }
+}
---
+++ Wk2_Challenges/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk2_Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //New up (instantiate) an isntance of your Greeter class inside Main method.  From this instance, call all the methods you've built.
+            Greeter greeter = new Greeter();
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            greeter.TakeNameSayHello(name);
+            greeter.TakeNameSayBye(name);
+            greeter.GreetByTimeOfDay();
+
+        }
+    }
+}
